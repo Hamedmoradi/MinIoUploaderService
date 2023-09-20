463c90e7c019c5636484bcd9070ab5f25c9b2316
hamed <hamedmoradi.mailsbox@gmail.com>
2023-09-20 17:48:41 +0330

    Initial commit

---
+++ src/main/java/minio/uploader/service/MinIoUploaderServiceApplication.java
@@ -0,0 +1,13 @@
+package minio.uploader.sesrvice.miniouploaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinIoUploaderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinIoUploaderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/minio/uploader/service/MinIoUploaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package minio.uploader.sesrvice.miniouploaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinIoUploaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
